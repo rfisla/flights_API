46501ccb39fd18c13ca885b492adc005eff2a17c
rfisla <rubenfernandezisla@gmail.com>
2022-03-31 18:28:48 +0200

    host change

--- src/back/main.py
+++ src/back/main.py
@@ -66,4 +66,5 @@ def get_results():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="https://api-cheapflights.herokuapp.com")
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
