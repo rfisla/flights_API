80d7f974c10a32f29e7608ec393ba951f6f845f0
Rubén Fernández Isla <58310013+rfisla@users.noreply.github.com>
2022-04-01 13:21:31 +0200

    Update front_utils.py

--- src/interactive/front_utils.py
+++ src/interactive/front_utils.py
@@ -2,12 +2,12 @@ import requests
 
 
 def get_request(request):
-    url = "https://api-cheapflights.herokuapp.com/streamlit-request"
+    url = "http://api-cheapflights.herokuapp.com/streamlit-request"
     response = requests.get(url, params=request)
     return response.json()
 
 
 def get_results(params):
-    url = "https://api-cheapflights.herokuapp.com/api_call"
+    url = "http://api-cheapflights.herokuapp.com/api_call"
     response = requests.get(url, params=params)
     return response.json()
