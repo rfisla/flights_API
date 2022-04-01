85b4274fd41b62175bddbbbd8e417dc5e3b52328
Rubén Fernández Isla <58310013+rfisla@users.noreply.github.com>
2022-04-01 14:25:59 +0200

    Update main.py

--- src/back/main.py
+++ src/back/main.py
@@ -66,5 +66,5 @@ def get_results():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT")))
 
